d7d895ea6d77a8801e36cac9f15d34c6ef21d46b
Mishe <konmishe9@gmail.com>
2026-04-22 06:13:04 -0700

    Remove bootstrap JS import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import "../bootstrap/css/bootstrap.min.css"
-import "../bootstrap/js/bootstrap.min.js"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
